c849d8f2c121c8e905172f9f2519fab774234d67
Emily <emilypbi@gmail.com>
2023-06-16 15:54:08 +0100

    Remove OOP structure. Restructure data.

--- src/data.py
+++ src/data.py
@@ -1,14 +1,10 @@
 import pandas as pd
 
 
-class Data:
-    def __init__(self,
-                 column_names,
-                 data
-                 ):
-        self.column_names = column_names
-        self.data = data
+def create_data(data):
+    df = pd.DataFrame(data)
+    return df
 
-    def create_data(self):
-        df = pd.DataFrame(self.data, self.column_names)
-        return df
+
+def normalise_data(data_to_normalise):
+    pass
